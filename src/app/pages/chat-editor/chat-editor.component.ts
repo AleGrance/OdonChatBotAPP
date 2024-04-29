e8aeeb4c4c365e51c092ef5e71be69fdd440f0c1
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2024-04-29 11:38:27 -0400

    add formBuilder to chat-editor

--- src/app/pages/chat-editor/chat-editor.component.ts
+++ src/app/pages/chat-editor/chat-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ChatsService } from 'src/app/services/chats.service';
 
 @Component({
   selector: 'app-chat-editor',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatEditorComponent implements OnInit {
 
-  constructor() { }
+  public chatForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, private chatsService: ChatsService) {
+    this.chatForm = this.formBuilder.group({
+      mensaje: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  enviarMensaje() {
+    const newMessage = this.chatForm.value;
+
+    if (!this.chatForm.valid) {
+      alert('Debe completar los campos necesarios');
+      return;
+    }
+
+    // console.log(this.chatForm.valid);
+    console.log('El mensaje a enviar es: ', newMessage.mensaje);
+
+    this.chatsService.agregarChat(newMessage.mensaje);
+
+    this.chatForm.reset();
   }
 
 }
