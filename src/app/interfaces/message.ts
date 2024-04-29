43c0f30ae3152809adbd5321113aa2b8555f550f
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2024-04-29 11:51:09 -0400

    add Message interface

---
+++ src/app/interfaces/message.ts
@@ -0,0 +1,5 @@
+export interface Message {
+  texto: string,
+  fecha_hora: Date
+  from_id: number
+}
--- src/app/pages/chat-content/chat-content.component.ts
+++ src/app/pages/chat-content/chat-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chat } from 'src/app/interfaces/chat';
+import { ChatsService } from 'src/app/services/chats.service';
 
 @Component({
   selector: 'app-chat-content',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-content.component.scss']
 })
 export class ChatContentComponent implements OnInit {
+  public chatsArray: Chat[] = [];
 
-  constructor() { }
+  constructor(private chatsService: ChatsService) { }
 
   ngOnInit(): void {
+    this.chatsService.obtenerChats.subscribe(result => {
+      this.chatsArray = result;
+      console.log('Los mensajes desde el compo chat-content:', result);
+    })
   }
 
 }
